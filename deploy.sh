564953647556bd8b787d195a9ac181bd318f50c5
Eugene <evgen7661@gmail.com>
2021-10-15 12:51:01 +0300

    vue deploy files

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:pulesos/pulesos_dev_form_vue.git master:gh-pages
+ git push -f git@github.com:pulesos/pulesos_dev_form_vue.git main:gh-pages
 
 cd -
