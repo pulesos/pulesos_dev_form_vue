80bdf125007b0563ec041245b94e77b745558ffb
Eugene <evgen7661@gmail.com>
2021-10-15 13:12:01 +0300

    first commit

---
+++ precache-manifest.a08bd44bef2f387add60d60acc5ed9bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2e5be57153fb2f945f",
+    "url": "/pulesos_dev_form_vue/css/app.e21f3b91.css"
+  },
+  {
+    "revision": "5b01018d12c8a355c95b4cb679a89174",
+    "url": "/pulesos_dev_form_vue/index.html"
+  },
+  {
+    "revision": "ac2e5be57153fb2f945f",
+    "url": "/pulesos_dev_form_vue/js/app.dd93050e.js"
+  },
+  {
+    "revision": "5f6f8a886496f5002ca5",
+    "url": "/pulesos_dev_form_vue/js/chunk-vendors.42c06d9c.js"
+  },
+  {
+    "revision": "cb5439daa1c07603ea126639a45b975e",
+    "url": "/pulesos_dev_form_vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pulesos_dev_form_vue/robots.txt"
+  }
+]);
